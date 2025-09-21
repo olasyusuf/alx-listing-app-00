de525e205f624e200f462a5e366859206d8db5ad
Olalekan Yusuf <olasyusuf@gmail.com>
2025-09-21 22:30:28 +0100

    feat: Add Property Listing Data

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -13,3 +13,23 @@ export interface ButtonProps {
   onClick?: () => void;
   className?: string;
 }
+
+// interfaces/index.ts
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
